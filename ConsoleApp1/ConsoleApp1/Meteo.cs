e972d4f0080515d604bb81cfca95c1a068669e10
Ksoucha <ksenia.fin.03@gmail.com>
2023-09-19 10:16:08 -0400

    ajout outil meteo

---
+++ ConsoleApp1/ConsoleApp1/Meteo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Meteo
+    {
+        public Meteo(double temperatureCelcius) 
+        {
+            Console.WriteLine("Bienvenue à l'outil Meteo");
+
+            Console.WriteLine("Quelle est la temperature en Celcius aujourdhui?");
+            float temperatureCelcius;
+
+            Console.Write("La temperature en Fahrenheit: ");
+
+            _ = temperatureCelcius * 1.8 + 32;
+        }
+    }
+}
